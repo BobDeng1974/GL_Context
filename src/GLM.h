6bcb75ab37207d9de2f2ee7f228aaa0c724b8acb
Jas <Jas@Jass-MacBook-Pro.local>
2017-06-01 19:33:17 -0400

    Added all core functionality

---
+++ src/GLM.h
@@ -0,0 +1,17 @@
+//
+//  GLM.h
+//  Bobo
+//
+//  Created by Jas S on 2017-02-04.
+//  Copyright © 2017 Jas S. All rights reserved.
+//
+
+#ifndef GLM_h
+#define GLM_h
+
+#define GLM_ENABLE_EXPERIMENTAL
+#include "../../Libraries/glm/glm/glm.hpp"
+#include "../../Libraries/glm/glm/gtx/transform.hpp"
+#include "../../Libraries/glm/glm/gtx/quaternion.hpp"
+
+#endif /* GLM_h */
